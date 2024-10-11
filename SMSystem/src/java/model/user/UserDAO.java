fa358c9b6a6427588af3fede140f0b563e87c99d
an-rias01 <anlttse183723@fpt.edu.vn>
2024-10-11 08:31:59 +0700

    Create User Controller

--- SMSystem/src/java/model/user/UserDAO.java
+++ SMSystem/src/java/model/user/UserDAO.java
@@ -22,27 +22,29 @@ import utils.DBUtils;
  * @author LENOVO
  */
 public class UserDAO {
-    private static final String LOGIN = "SELECT userName,fullName,userId,phoneNumber,sex,email,isActive,roleId,createdAt\n" +
-"FROM users\n" +
-"WHERE (userName =? OR email=?) AND password = ?";
-     private static final String GET_ALL_USER = "SELECT userId,userName, fullName, roleId FROM users WHERE fullName LIKE ? ";
-    private static final String UPDATE = "UPDATE users set fullName= ?, userName=?, password=?, phoneNumber=?, sex=?, email=?,roleId=? where userName=?";
 
-    
+    private static final String LOGIN = "SELECT userName,fullName,userId,phoneNumber,sex,email,isActive,roleId,createdAt\n"
+            + "FROM users\n"
+            + "WHERE (userName =? OR email=?) AND password = ?";
+    private static final String GET_ALL_USER = "SELECT userId,userName, fullName, roleId FROM users WHERE fullName LIKE ? ";
+    private static final String UPDATE = "UPDATE users set fullName= ?, userName=?, password=?, phoneNumber=?, sex=?, email=?,roleId=? where userName=?";
+    private static final String CHECK_USERNAME_EXISTS = "SELECT COUNT(*) FROM users WHERE userName = ?";
+    private static final String INSERT_USER = " INSERT INTO users (userName, fullName, password, phoneNumber, sex, email, isActive, roleId, createdAt) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)";
+
     public UserDTO checkLogin(String userIndentify, String password) throws SQLException, ClassNotFoundException, NamingException {
         UserDTO user = null;
         Connection conn = null;
         PreparedStatement ptm = null;
         ResultSet rs = null;
-        try{
+        try {
             conn = DBUtils.getConnection();
-            if(conn!=null){
+            if (conn != null) {
                 ptm = conn.prepareStatement(LOGIN);
                 ptm.setString(1, userIndentify);
                 ptm.setString(2, userIndentify);
                 ptm.setString(3, password);
                 rs = ptm.executeQuery();
-                if(rs.next()){
+                if (rs.next()) {
                     int userId = rs.getInt("userId");
                     String userName = rs.getString("userName");
                     String fullName = rs.getString("fullName");
@@ -56,15 +58,21 @@ public class UserDAO {
                     user = new UserDTO(userId, fullName, userName, password, phoneNumber, sex, email, isActive, roleId, createdAt);
                 }
             }
-        }finally{
-            if(rs!=null) rs.close();
-            if(ptm!=null) ptm.close();
-            if(conn!=null) rs.close();
+        } finally {
+            if (rs != null) {
+                rs.close();
+            }
+            if (ptm != null) {
+                ptm.close();
+            }
+            if (conn != null) {
+                rs.close();
+            }
         }
-        
+
         return user;
     }
-    
+
     public List<UserDTO> getAllUser() throws ClassNotFoundException, SQLException {
         List<UserDTO> userList = new ArrayList<>();
         Connection conn = null;
@@ -102,10 +110,10 @@ public class UserDAO {
             if (conn != null) {
                 conn.close();
             }
-        } 
+        }
         return userList;
     }
-    
+
     public boolean userAfterUpdate(UserDTO user) {
         boolean check = false;
         Connection conn = null;
@@ -115,19 +123,66 @@ public class UserDAO {
         try {
             conn = DBUtils.getConnection();
             if (conn != null) {
-                ptm = conn.prepareStatement(UPDATE);
+                ptm = conn.prepareStatement(INSERT_USER);
                 ptm.setString(1, user.getFullName());
                 ptm.setString(2, user.getUserName());
                 ptm.setString(3, user.getPassword());
-                ptm.setString(4, user.getPhoneNumber());
-                ptm.setString(5, user.getSex());
-                ptm.setString(6, user.getEmail());
-                check = ptm.executeUpdate() >0 ?true:false;
+                ptm.setString(4, user.getEmail());
+                check = ptm.executeUpdate() > 0 ? true : false;
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
         return check;
     }
+
+    public boolean isUserNameExists(String userName) throws SQLException, ClassNotFoundException {
+        try (Connection conn = DBUtils.getConnection();
+                PreparedStatement stmt = conn.prepareStatement(CHECK_USERNAME_EXISTS)) {
+            stmt.setString(1, userName);
+            try (ResultSet rs = stmt.executeQuery()) {
+                if (rs.next()) {
+                    int count = rs.getInt(1);
+                    return count > 0;
+                }
+            }
+        }
+        return false;
+    }
    
+
+    public boolean createUser(UserDTO user) throws SQLException, ClassNotFoundException {
+        boolean check = false;
+        Connection conn = null;
+        PreparedStatement ptm = null;
+        try {
+//userName,fullName,password,phoneNumber,sex,email,isActive,roleId,createdAt)
+            conn = DBUtils.getConnection();
+            if (conn != null) {
+                ptm = conn.prepareStatement(INSERT_USER);
+                ptm.setString(1, user.getUserName());
+                ptm.setString(2, user.getFullName());
+                ptm.setString(3, user.getPassword());
+                ptm.setString(4, user.getPhoneNumber());
+                ptm.setString(5, user.getSex());
+                ptm.setString(6, user.getEmail());
+                ptm.setBoolean(7, user.isActive());
+                ptm.setString(8, user.getRoleId());
+                ptm.setDate(9, (Date) user.getCreatedAt());
+                check = ptm.executeUpdate() > 0;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (ptm != null) {
+                ptm.close();
+            }
+            if (conn != null) {
+                conn.close();
+            }
+
+        }
+        return check;
+    }
+
 }
---
+++ SMSystem/src/java/model/user/UserError.java
@@ -0,0 +1,84 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model.user;
+
+
+public class UserError {
+    private String userNameError;
+    private String fullNameError;
+    private String roleIDError;
+    private String passwordError;
+    private String confirmError;
+    private String error;
+
+    public UserError(String userNameError, String fullNameError, String roleIDError, String passwordError, String confirmError, String error) {
+        this.userNameError = userNameError;
+        this.fullNameError = fullNameError;
+        this.roleIDError = roleIDError;
+        this.passwordError = passwordError;
+        this.confirmError = confirmError;
+        this.error = error;
+    }
+
+    public String getUserNameError() {
+        return userNameError;
+    }
+
+    public void setUserNameError(String userNameError) {
+        this.userNameError = userNameError;
+    }
+
+    public String getFullNameError() {
+        return fullNameError;
+    }
+
+    public void setFullNameError(String fullNameError) {
+        this.fullNameError = fullNameError;
+    }
+
+    public String getRoleIDError() {
+        return roleIDError;
+    }
+
+    public void setRoleIDError(String roleIDError) {
+        this.roleIDError = roleIDError;
+    }
+
+    public String getPasswordError() {
+        return passwordError;
+    }
+
+    public void setPasswordError(String passwordError) {
+        this.passwordError = passwordError;
+    }
+
+    public String getConfirmError() {
+        return confirmError;
+    }
+
+    public void setConfirmError(String confirmError) {
+        this.confirmError = confirmError;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public void setError(String error) {
+        this.error = error;
+    }
+
+    public UserError() {
+        userNameError="";
+        fullNameError="";
+        roleIDError="";
+        passwordError="";
+        confirmError="";
+        error="";
+    }
+
+   
+}
